b56ecf1a8e04fd7c579dace914667cb3fb3706da
thanhthai3010 <n.thanhthai3010@hotmail.com>
2015-12-02 17:31:53 +0700

    Update service

--- src/main/java/app/server/handling/ServerInterf.java
+++ src/main/java/app/server/handling/ServerInterf.java
@@ -2,6 +2,8 @@ package app.server.handling;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.List;
+
 import app.utils.dto.FacebookData;
 import app.utils.dto.FacebookDataToInsertDB;
 import app.utils.dto.ListPieData;
@@ -12,21 +14,44 @@ public interface ServerInterf extends Remote {
 
 	public String hello() throws RemoteException;
 	
+	/**
+	 * thaint
+	 * Save facebook data into database
+	 * @param fbDataToInsertDB
+	 * @throws RemoteException
+	 */
 	public void saveFBData(FacebookDataToInsertDB fbDataToInsertDB) throws RemoteException;
 	
-	public double runAnalyzeSentiment(String inputText, boolean isNeedToCheck) throws RemoteException;
+//	public double runAnalyzeSentiment(String inputText) throws RemoteException;
 	
-	public String[] runSpellCheckAndToken(String inputText) throws RemoteException;
+//	public String[] runSpellCheckAndToken(String inputText) throws RemoteException;
 	
-	//TODO
 	/**
+	 * thaint
 	 * Processing data for LDA Model
 	 * @param input List of post and comment
 	 * @throws RemoteException
 	 */
 	public void processLDA(FacebookData input) throws RemoteException;
 	
+	/**
+	 * thaint
+	 * Get list topic to draw word-cloud
+	 * @return ListTopic
+	 * @throws RemoteException
+	 */
 	public ListTopic getDescribleTopics() throws RemoteException;
 	
 	public ListPieData processSentiment(int topicID) throws RemoteException;
+	
+	/**
+	 * This function will get data of facebook using list pageID input and dates
+	 * @param lstPageID list page ID
+	 * @param startDate date
+	 * @param endDate date
+	 * @return
+	 * @throws RemoteException
+	 */
+	public FacebookData getFBDataByPageIDAndDate(List<String> lstPageID, String startDate,
+			String endDate) throws RemoteException;
 }
