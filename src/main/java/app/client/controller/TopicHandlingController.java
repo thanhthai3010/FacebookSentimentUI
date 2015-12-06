c0d2704613e5707561512528bc2eca7bbe3bec82
thanhthai3010 <n.thanhthai3010@hotmail.com>
2015-12-06 17:49:25 +0700

    Add function draw pie chart

--- src/main/java/app/client/controller/TopicHandlingController.java
+++ src/main/java/app/client/controller/TopicHandlingController.java
@@ -1,6 +1,9 @@
 package app.client.controller;
 
 import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
@@ -11,7 +14,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import app.utils.dto.FacebookData;
+import app.utils.dto.JsonDataToDrawChart;
+import app.utils.dto.ListPieData;
 import app.utils.dto.ListTopic;
+import app.utils.dto.PieChart;
+import app.utils.dto.PieData;
 
 @Controller
 public class TopicHandlingController {
@@ -21,34 +29,171 @@ public class TopicHandlingController {
 
 	private ListTopic describeTopics;
 	
-	@ResponseBody
-	@RequestMapping(value = "/topicID", method = RequestMethod.GET, produces = "text/html; charset=utf-8")
-	public String getPieData(Model model, HttpServletRequest request)
+	/**
+	 * Neutral Sentiment
+	 */
+	private static final int NEUTRAL = 0;
+
+	/**
+	 * Negative Sentiment
+	 */
+	private static final int NEGATIVE = -1;
+
+	/**
+	 * Positive Sentiment
+	 */
+	private static final int POSITIVE = 1;
+	
+	/**
+	 * Process LDA and output topics
+	 * @param model
+	 * @param req
+	 * @return
+	 * @throws RemoteException
+	 */
+	@RequestMapping(value = "/startAnalysis", method = RequestMethod.POST)
+	public String startAnalysisProcess(Model model, HttpServletRequest req,
+			String pageID, String dateFrom, String dateTo)
 			throws RemoteException {
+		logger.info("View word cloud of topic");
+		
+		boolean errorFlag = false;
+		
+		String[] lstPageIDFromClient = pageID.split(",");
+		
+		List<String> lstPageID = new ArrayList<String>();
+		for (String pageItem : lstPageIDFromClient) {
+			lstPageID.add(pageItem);
+		}
+		//TODO
+		// please check value of getFBDataByPageIDAndDate() method return.
+		// If we does not have data, show message for user.
+		FacebookData inputDataForService = new FacebookData();
+		inputDataForService = HomeController.server.getFBDataByPageIDAndDate(
+				lstPageID, dateFrom, dateTo);
+		
+		if (inputDataForService.getFbDataForService().isEmpty()) {
+			errorFlag = true;
+		}
+		
+		HomeController.server.processLDA(inputDataForService);
 
-		// first of all, we need to get list of comment to process sentiment
-
-		return null;
-
+		if (errorFlag) {
+			model.addAttribute("error", "Please select another data");
+			return "analysisData";
+		} else {
+			return "viewTopics";
+		}
 	}
 	
-	@RequestMapping(value = "/processLDA", method = RequestMethod.POST)
-	public String listTopicsView(Model model) {
-		return "viewTopics";
-
-	}
-
+	/**
+	 * Display topic word-cloud
+	 * @param model
+	 * @param req
+	 * @return
+	 * @throws RemoteException
+	 */
 	@ResponseBody
 	@RequestMapping(value = "/getListTopic", method = RequestMethod.GET, produces = "text/html; charset=utf-8") 
 	public String getListTopicsView(Model model, HttpServletRequest req) throws RemoteException {
 		
-		logger.info("Read from Server");
+		logger.info("Starting draw word-cloud");
 		this.describeTopics = HomeController.server.getDescribleTopics();
 		
 		return describeTopics.toTopicsJson();
 	}
-
-	public void drawPieChart() {
+	
+	/**
+	 * View detail of each topic by topicID
+	 * @param model
+	 * @param request
+	 * @return
+	 * @throws RemoteException
+	 */
+	@ResponseBody
+	@RequestMapping(value = "/topicID", method = RequestMethod.GET, produces = "text/html; charset=utf-8")
+	public String getPieData(Model model, HttpServletRequest request)
+			throws RemoteException {
 		
+		int topicID = 1;
+		try {
+			topicID = Integer.parseInt(request.getParameter("id"));
+		} catch (Exception e) {
+			logger.info("Has problem here");
+		}
+		// first of all, we need to get list of comment to process sentiment
+		ListPieData affterSentiment = HomeController.server.processSentiment(topicID);
+		
+		/**
+		 * get data to draw pie chart
+		 */
+		List<PieChart> listPieChart = getCharData(affterSentiment);
+		
+		// convert to JSon
+		String listDetailData = affterSentiment.pieDataToJson();
+		
+		String pieChartJson = PieChart.pieChartsToJson(listPieChart);
+		
+		JsonDataToDrawChart jsonData = new JsonDataToDrawChart(listDetailData, pieChartJson);
+		
+		return jsonData.toJsonData();
+
+	}
+	
+	@RequestMapping(value = "/pieChart", method = RequestMethod.GET, produces = "text/html; charset=utf-8")
+	public String callDrawPieChart(Model model, HttpServletRequest request)
+			throws RemoteException {
+		
+		int topicID = 1;
+		try {
+			topicID = Integer.parseInt(request.getParameter("id"));
+		} catch (Exception e) {
+			logger.info("Has problem here");
+		}
+		model.addAttribute("topicID", topicID);
+		
+		
+		return "detailOfTopic";
+
+	}
+	
+	/**
+	 * Object to draw pie chart
+	 * @param lisPieData
+	 * @return
+	 */
+	public List<PieChart> getCharData(ListPieData lisPieData){
+		// loop all pieData input to calculate sum of each type color
+		
+		List<PieChart> lstPieChar = new ArrayList<PieChart>();
+		
+		int numOfPos = 0;
+		int numOfNeg = 0;
+		int numOfNeu = 0;
+		for (PieData pieData : lisPieData) {
+			switch (pieData.getTypeColor()) {
+			case POSITIVE:
+				numOfPos++;
+				break;
+			case NEGATIVE:
+				numOfNeg++;
+				break;
+			case NEUTRAL: 
+				numOfNeu++;
+				break;
+			default:
+				break;
+			}
+		}
+		
+		PieChart piePositive = new PieChart("Positive Percent", numOfPos);
+		PieChart pieNegative = new PieChart("Negative Percent", numOfNeg);
+		PieChart pieNeutral = new PieChart("Neutral Percent", numOfNeu);
+		
+		lstPieChar.add(piePositive);
+		lstPieChar.add(pieNegative);
+		lstPieChar.add(pieNeutral);
+		
+		return lstPieChar;
 	}
 }
